667bd268a15829039fa8dcafc536e464d2a5331a
Eikaros <eikaros4@gmail.com>
2021-04-21 21:37:18 +0700

    Swagger

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import rootofequa from './routes/RofEq.js'
 import liner from './routes/Linal.js'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+import swaggerUi from 'swagger-ui-express'
+const swaggerDocument = require('./swagger.json')
 import interpo from './routes/Inter.js'
 import regress from './routes/Regres.js'
 const app = express()
@@ -14,6 +18,11 @@ app.use('/api/Lina', liner)
 app.use('/api/Rof', rootofequa)
 app.use('/api/Inter', interpo)
 app.use('/api/Reg', regress)
+app.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, { explorer: true })
+)
 app.listen(port, 'localhost', () => {
     console.log(`server Started at port ${port}`)
 })
